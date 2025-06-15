f4242ca7c794c32466a64654bfe162bdf3f73fd1
Toissincera <uzmanqaisar12@gmail.com>
2025-06-15 06:00:58 +0530

    no kings, no constants

--- supabase/supabase.js
+++ supabase/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import Constants from "expo-constants";
+// import Constants from "expo-constants";
 
 const supabaseUrl = "https://lcfoigtqlfchauzgqsjw.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZm9pZ3RxbGZjaGF1emdxc2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3OTc1MzcsImV4cCI6MjA2NTM3MzUzN30.L-l8YAeHcO5-RBmB6ezYJfjSY6CBDJaDy_P93KEO2lw";
